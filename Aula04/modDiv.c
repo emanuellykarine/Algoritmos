54e6377d694d3423c002daa6251d120a214981ed
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2024-11-05 12:34:48 +0000

    modDiv

--- Aula04/modDiv.c
+++ Aula04/modDiv.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
 int mod(int a, int b){
-    b = b + b;
-    if (b >= a){
+    if (b > a){
         return a;
     } else {
-        return mod(a, b);
+        return mod(a-b, b);
+    }
+}
+
+int div(int a, int b){
+    if (b > a){
+        return 0;
+    } else {
+        return 1 + div(a-b, b);
     }
 }
 
 int main(){
-    printf("%d\n", mod(25, 4));
+    int resultadoResto = mod(26, 5);
+    int resultadoQuo = div(26, 5);
+    printf("Resto: %d\n", resultadoResto);
+    printf("Quociente: %d\n", resultadoQuo);
+    return 0;
 }
