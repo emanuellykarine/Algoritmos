35f3753b36e153f5736a5b751e64a97059aea4b6
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2024-11-07 18:34:09 +0000

    Campo minado finalizado

--- Aula04/campoMinado.c
+++ Aula04/campoMinado.c
@@ -1,49 +1,5 @@
 #include <stdio.h>
 
-char matriz(int linha, int coluna){
-      char matriz[5][5];
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            matriz[i][j] = 0;
-        }
-    }
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (matriz[i][j] == 0){
-                matriz[i][j] = '.';
-                printf("%c ", matriz[i][j]);
-            } 
-        }
-        printf("\n");
-    }
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (matriz[i][j] == 0){
-                matriz[i][j] = '.';
-            } else if (matriz[i][j] == -1) {
-                matriz[i][j] = '*';
-            }
-        }
-    }
-
-    matriz[0][1] = -1;
-    matriz[0][2] = -1;
-    matriz[1][0] = -1;
-    matriz[1][3] = -1;
-    matriz[2][3] = -1;
-    matriz[2][4] = -1;
-    matriz[3][1] = -1;
-    matriz[3][4] = -1;
-    matriz[4][0] = -1;
-    matriz[4][2] = -1;
-
-    
-
-}
-
 int main(){
     int matriz[5][5] = {
         {0, -1, -1, 0, 0}, //{2, -1, -1, 2, 1}
@@ -53,74 +9,107 @@ int main(){
         {-1, 0, -1, 0, 0}  //{-1, 3, -1, 2, 1}
     };
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (matriz[i][j] == 0) {
-                int a = i + 1;
-                int b = j + 1;
-                int c = i - 1;
-                int d = j - 1;
+    int matrizChar[5][5] = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
 
-                if (a <= 4){
-                    if (matriz[a][j] == -1){
-                         matriz[i][j] += 1;
-                    } 
+    int linha = 0;
+    int coluna = 0;
+    int cont = 0;
+    printf("CAMPO MINADO (Linha e coluna: 0 a 4)\n");
+
+    while (matriz[linha][coluna] != -1 && cont != 15){
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if (matrizChar[i][j] == 0){
+                    printf(". ");
+                } else {
+                    printf("%d ", matrizChar[i][j]);
                 }
+            }
+            printf("\n");
+        }
 
-                if (b <= 4){
-                    if (matriz[i][b] == -1){
-                        matriz[i][j] += 1;
-                    }  
-                }
+        printf("Digite a posição da linha e coluna: ");
+        scanf("%d %d", &linha, &coluna);
+        cont += 1;
+                if (matriz[linha][coluna] == 0){
+                    int a = linha + 1;
+                    int b = coluna + 1;
+                    int c = linha - 1;
+                    int d = coluna - 1;
 
-                if (a <= 4 && b <= 4){
-                    if (matriz[a][b] == -1){
-                        matriz[i][j] += 1;
-                    } 
-                }
-
-                if (c >= 0){
-                    if(matriz[c][j] == -1){
-                        matriz[i][j] += 1;
+                    if (a <= 4){
+                        if (matriz[a][coluna] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
                     }
-                }
 
-                if (d >= 0){
-                    if (matriz[i][d] == -1){
-                        matriz[i][j] += 1;
-                    } 
-                }
-
-                if (c >= 0 && d >= 0){
-                    if(matriz[c][d] == -1){
-                        matriz[i][j] +=1;
+                    if (b <= 4){
+                        if (matriz[linha][b] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
                     }
-                }
 
-                if (b <= 4 && c >= 0){
-                    if (matriz[c][b] == -1){
-                        matriz[i][j] += 1;
+                    if (a <= 4 && b <= 4){
+                        if (matriz[a][b] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
+                    }
+
+                    if (c >= 0){
+                        if (matriz[c][coluna] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
+                    }
+
+                    if (d >= 0){
+                        if (matriz[linha][d] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
+                    }
+
+                    if (c >= 0 && d >= 0){
+                        if (matriz[c][d] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
+                    }
+
+                    if (b <= 4 && c >= 0){
+                        if (matriz[c][b] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
+                    }
+
+                    if (a <= 4 && d >= 0){
+                        if (matriz[a][d] == -1){
+                            matrizChar[linha][coluna] += 1;
+                        }
                     }
                 } 
-
-                if (a <= 4 && d >= 0){
-                    if (matriz[a][d] == -1){
-                        matriz[i][j] += 1;
-                    }
-                }
-        
             }
-                
-        }
-    }
-    
+
     // Print matriz modificada
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            printf("%d ", matriz[i][j]);
+            if (matriz[i][j] == 0){
+                    printf(". ");
+                } else {
+                    printf("* ");
+                }
         }
         printf("\n");
     }
+    if (cont != 15) {
+        printf("Jogo terminado.Você perdeu :(\n");
+    } else {
+        printf("Você venceu ;)\n");
+    }
+    
 
     return 0;
 }
