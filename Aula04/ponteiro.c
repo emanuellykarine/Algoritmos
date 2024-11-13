2132e861a702fa52c6debfe74e73d07fdb339570
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2024-11-13 16:12:57 +0000

    ponteiro

---
+++ Aula04/ponteiro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *ponteiro = &a;
+    int valor = *ponteiro;
+    // com * - valor; sem * - endereço de memoria 
+    // ponteiro armazena o endereço de memoria de outra variavel
+    //se modifica o enedreço, modifica o valor 
+
+    printf("Valor: %d\n", valor);
+
+    return 0;
+}
