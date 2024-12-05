02c82d03eaaa347d859ef706d1057f699450295b
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2024-12-05 17:54:03 +0000

    Modificação struct contato

--- Struct/contato.c
+++ Struct/contato.c
@@ -30,64 +30,50 @@ void inserir(struct Contato agenda[], int cont){
 }
 
 void listar(struct Contato agenda[], int cont){
-    if (agenda == NULL){
-        printf("Lista de contatos vazia.");
-    } else {
-        printf("Lista de contatos:\n");
-            for (int i = 0; i < cont; i++){
-                printf("Contato %d\n", i + 1);
-                printf("Nome: %s  ", agenda[i].nome);
-                printf("Telefone: %s  ", agenda[i].telefone);
-                printf("Email: %s  ", agenda[i].email);
-                printf("Endereço: %s\n", agenda[i].endereco);
-            }
+    printf("Lista de contatos:\n");
+    for (int i = 0; i < cont; i++){
+        printf("Contato %d\n", i + 1);
+        printf("Nome: %s  ", agenda[i].nome);
+        printf("Telefone: %s  ", agenda[i].telefone);
+        printf("Email: %s  ", agenda[i].email);
+        printf("Endereço: %s\n", agenda[i].endereco);
     }
 }
+
 
 void excluir(struct Contato agenda[], int opContato){
     int opcao = opContato - 1;
-    if (opcao < 0){
-        printf("Não tem contatos cadastrados.");
-    } else if (&agenda[opcao] == NULL){
-        printf("Não existe esse contato.");
-    } else {
-        for (int i = opcao;i < sizeof(agenda) - 1; ++i){ 
-            agenda[i] = agenda[i + 1];
-        }
+    for (int i = opcao;i < sizeof(agenda) - 1; ++i){ 
+        agenda[i] = agenda[i + 1];
     }
 }
 
+
 int atualizar(struct Contato agenda[], int opContato){
     int opcao = opContato - 1;
-
-    if (opcao < 0){
-        printf("Não tem contatos cadastrados.");
-    } else if (&agenda[opcao] == NULL){
-        printf("Não existe esse contato.");
-    } else {
-        struct Contato* contato = &agenda[opcao];
+    struct Contato* contato = &agenda[opcao];
         
-         printf("Digite o nome do contato: ");
-        scanf("%s", agenda[opcao].nome);
+    printf("Digite o nome do contato: ");
+    scanf("%s", agenda[opcao].nome);
 
-        scanf("%*c");
+    scanf("%*c");
 
-        printf("Digite o telefone do contato: ");
-        scanf("%s", agenda[opcao].telefone);
+    printf("Digite o telefone do contato: ");
+    scanf("%s", agenda[opcao].telefone);
 
-        scanf("%*c");
+    scanf("%*c");
 
-        printf("Digite o email do contato: ");
-        scanf("%s", agenda[opcao].email);
+    printf("Digite o email do contato: ");
+    scanf("%s", agenda[opcao].email);
 
-        scanf("%*c");
+    scanf("%*c");
 
-        printf("Digite o endereço do contato: ");
-        scanf("%s", agenda[opcao].endereco); 
+    printf("Digite o endereço do contato: ");
+    scanf("%s", agenda[opcao].endereco); 
 
-        scanf("%*c");
-    }  
-}
+    scanf("%*c");
+}  
+
 
 int main() {
     struct Contato agenda[100];
@@ -97,15 +83,19 @@ int main() {
     int opContato = 0;
     
     while (op != 10){
+        printf("------------------------------\n");
         printf("1 - Inserir contato \n2 - Atualizar contato \n3 - Excluir Contato \n4 - Listar contatos \n10 - Finalizar lista\n");
+        printf("------------------------------\n");
         printf("Digite a opção: ");
         scanf("%d", &op);
 
         if (op == 1 && cont < sizeof(agenda)){
+            printf("Inserir Contato\n");
             inserir(agenda, cont);
             cont += 1;
 
         } else if (op == 2){
+            printf("Atualizar Contato\n");
             listar(agenda, cont);
 
             printf("Qual contato você deseja atualizar as informações?");
@@ -115,6 +105,7 @@ int main() {
             printf("Contato atualizado.\n");
 
         } else if (op == 3){
+            printf("Excluir Contato\n");
             listar(agenda, cont);
 
             printf("Qual contato você deseja excluir?");
