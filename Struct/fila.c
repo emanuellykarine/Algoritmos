41838e6bcca53ec7ed826b114fa2360781610199
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2024-12-11 18:35:21 +0000

    inserir pessoas fila

--- Struct/fila.c
+++ Struct/fila.c
@@ -7,26 +7,63 @@ struct Pessoa{
     char preferencial[50];
 };
 
-struct FilaPreferencial {
+struct Fila {
     struct Pessoa* pessoaPonteiro = (struct Pessoa*) malloc(sizeof(struct Pessoa));
-    struct Pessoa pessoa;
+    struct Pessoa pessoa[100];
 };
 
-struct FilaNormal{
-    struct Pessoa* pessoaPonteiro = (struct Pessoa*) malloc(sizeof(struct Pessoa));
-    struct Pessoa pessoa;
-};
+void inserirPessoaPreferencial(struct Fila* fila){
+    printf("Nome: ");
+    scanf("%s", fila->pessoaPonteiro->nome);
 
+    printf("Idade: ");
+    scanf("%d", fila->pessoaPonteiro->idade);
+
+    printf("Atendimento: ");
+    scanf("%s", fila->pessoaPonteiro->atendimento)
+
+    printf("Preferencial: ");
+    scanf("%s", fila->pessoaPonteiro->preferencial);
+
+    fila->pessoaPonteiro++;
+}
+
+void inserirPessoaNormal(struct Fila* fila){
+    printf("Nome: ");
+    scanf("%s", fila->pessoaPonteiro->nome);
+
+    printf("Idade: ");
+    scanf("%d", fila->pessoaPonteiro->idade);
+
+    printf("Atendimento: ");
+    scanf("%s", fila->pessoaPonteiro->atendimento)
+
+    printf("Preferencial: ");
+    scanf("%s", fila->pessoaPonteiro->preferencial);
+
+    fila->pessoaPonteiro++;
+}
+
+void removerPessoaPreferencial(struct Fila* fila, char* nome){
+    
+}
 
 int main(){
+    struct Fila filaPrefencial;
+    filaPrefencial.pessoaPonteiro = filaPrefencial.pessoa;
+    
+    struct Fila filaNormal;
+    filaNormal.pessoaPonteiro = filaNormal.pessoa;
     int op = 0;
 
     while (op != 100){
-        printf("1 - Inserir pessoa | 2 - Remover pessoa | 3 - Atender | 4 - Listar | 100 - Sair");
+        printf("1 - Inserir pessoa preferencial | 2 - Inserir pessoa na fila normal | 3 - Remover uma pessoa da fila preferencial | 4 - Remover uma pessoa da fila normal | 5 - Atender | 6 - Listar filas | 100 - Sair");
         if (op == 1){
-            
-    
-            inserirPessoa();
+            inserirPessoaPreferencial(&filaPrefencial);
+        } else if (op == 2){
+            inserirPessoaNormal(&filaNormal);
+        } else if (op == 3){
+
         }
     }
 }
