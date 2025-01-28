273086a59f771fb0dd65dc7593955e5980724e29
Emanuelly Karine Fernandes dos Santos <20241014040009@ifrn.local>
2025-01-28 08:27:56 -0300

    alterações selection e insertion

--- Alg_ordenação/selectionSort.java
+++ Alg_ordenação/selectionSort.java
@@ -1,33 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.io.FileReader;
 
 public class selectionSort{
     public static void main(String[] args){
-        ArrayList<Integer> lista = new ArrayList<Integer>(); //Criação da lista para guardar os valores
-        try(BufferedReader br = new BufferedReader(new FileReader("100000Numeros.txt"))){ //leitura do arquivo
+        List<Integer> lista = new ArrayList<>(); //Criação da lista para guardar os valores
+        try(BufferedReader br = new BufferedReader(new FileReader("100Numeros.txt"))){ //leitura do arquivo
             String line = br.readLine(); //linha por linha
             while (line != null){
                 lista.add(Integer.parseInt(line)); //guarda linha na lista transformando para inteiro
                 line = br.readLine();     
             }
 
+            int[] array = lista.stream().mapToInt(i -> i).toArray();
+
             long tempoInicial = System.currentTimeMillis(); //tempo inicial antes da execução do código
-            for (int i = 0; i < lista.size(); i++){ //vai até o ultimo
+            for (int i = 0; i < array.length; i++){ //vai até o ultimo
                 int indiceDoMenor = i;
-                for (int j = i + 1; j < lista.size(); j++){ // começa do numero da frente e vai ate o final
-                    if (lista.get(j) < lista.get(indiceDoMenor)){ //se for menor
+                for (int j = i + 1; j < array.length; j++){ // começa do numero da frente e vai ate o final
+                    if (array[j] < array[indiceDoMenor]){ //se for menor
                         indiceDoMenor = j; //indice do menor recebe indice do atual 
                     }
                 }
                 //Depois que acha o menor de todos, traz ele pra frente
-                int guarda = lista.get(i); //recebe valor de comparação inicial
-                lista.set(i, lista.get(indiceDoMenor)); //guarda o menor valor no indice inicial
-                lista.set(indiceDoMenor, guarda); //guardar o valor de comparação inicial na posição antiga do menor
+                int guarda = array[i]; //recebe valor de comparação inicial
+                array[i] = array[indiceDoMenor]; //guarda o menor valor no indice inicial
+                array[indiceDoMenor] = guarda; //guardar o valor de comparação inicial na posição antiga do menor
                 
             }
             System.out.print("tempo de execução:" + (System.currentTimeMillis() - tempoInicial) + " milisegundos \n"); 
+            for (int i = 0; i < array.length; i++){
+                System.out.println(array[i]);
+            }
         } catch (IOException e){
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         }
