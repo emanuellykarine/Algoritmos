ca6ad3b4b9e5ef095b171adaba457855477c37b9
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2025-02-02 20:28:07 +0000

    modificações

--- Alg_ordenação/insertionSort.java
+++ Alg_ordenação/insertionSort.java
@@ -7,22 +7,15 @@ import java.io.FileReader;
 public class insertionSort{
     public static void main(String[] args){
         List<Integer> lista = new ArrayList<>();
-<<<<<<< HEAD
         try(BufferedReader br = new BufferedReader(new FileReader(args[0]))){ //leitura do arquivo
             String line = br.readLine(); //linha por linha
             
-=======
-        try(BufferedReader br = new BufferedReader(new FileReader("100Numeros.txt"))){ //leitura do arquivo
-            String line = br.readLine(); //linha por linha
-            int a = 0;
->>>>>>> 273086a59f771fb0dd65dc7593955e5980724e29
             while (line != null){
                 lista.add(Integer.parseInt(line)); //guarda linha na lista transformando para inteiro
                 line = br.readLine();     
             }
 
             int[] array = lista.stream().mapToInt(i -> i).toArray();
-<<<<<<< HEAD
 
             long tempoInicial = System.currentTimeMillis(); //tempo inicial antes da execução do código
             for (int i = 1; i < array.length; i++){ //Vai até o final da lista
@@ -35,19 +28,6 @@ public class insertionSort{
                   // Caso o valor seja maior que o anterior, ele fica na posição a frente do menor valor  
 
                 array[j+1] = valor; //Guarda o valor no pedaço do array ja ordenado 
-=======
-
-            long tempoInicial = System.currentTimeMillis(); //tempo inicial antes da execução do código
-            for (int i = 1; i < array.length; i++){ //vai até o final da lista
-                int valor = array[i]; //pega o valor e usa como principal de comparação
-                int j = i - 1; //j começa do anterior
-                while (j >= 0 && valor < array[j]){ //enquanto j for maior ou igual a 0 e valor for menor
-                    array[j+1] = array[j]; //empurra o maior pra frente
-                    j = j - 1; //diminui do indice j
-                }
-
-                array[j+1] = valor; //guarda o menor valor na posição que restou antes dos maiores 
->>>>>>> 273086a59f771fb0dd65dc7593955e5980724e29
             }
 
             System.out.print("tempo de execução:" + (System.currentTimeMillis() - tempoInicial) + " milisegundos \n"); 
@@ -56,4 +36,4 @@ public class insertionSort{
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         }
     }        
-} 
+}
